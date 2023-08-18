863ed9215a0aca9abf1cea09bf8b45729c33fc13
joaoand <joao.demata@correo.unimet.edu.ve>
2023-08-18 19:39:49 -0400

    Crear observable para las imagenes

--- components/random_fox.tsx
+++ components/random_fox.tsx
@@ -1,11 +1,35 @@
-import { useRef } from 'react';
+import { useRef, useEffect, useState, use } from 'react';
 
 type Props = {image: string};
 
 export const RandomFox = ({ image }: Props) : JSX.Element => {
     const node = useRef<HTMLImageElement>(null);
-    
-    return <img ref={node} width={320} height='auto' src={image} className='rounded'/>
+    const [src, setSrc] = useState("data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4=");
+
+    useEffect(()=>{
+        // Nuevo observador 
+        const observer = new window.IntersectionObserver((entries)=>{
+            // OnIntersection => console.log
+        entries.forEach(_entry => {
+            if(_entry.isIntersecting){
+                console.log('Hey you');
+                setSrc(image)
+            }
+        })
+        });
+        // Observer node 
+        if(node.current){
+            observer.observe(node.current);
+        }
+
+        return () => {
+            observer.disconnect()
+        }
+    }, [image])
+
+  
+    // Desconectar
+    return <img ref={node} width={320} height='auto' src={src} className='rounded bg-gray-300'/>
 }
 
 // import type { FunctionComponent, FC } from 'react';
